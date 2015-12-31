c2db11156cd76c62899be08ce2907fd9cf7ae2de
Benjamin Smith <benjamindsmith3@gmail.com>
2015-12-31 15:06:25 -0500

    Update 12/31
    - Updated express-sessions that made the cookie-parser obsolete.
    - Enabled the Nginx to self sign SSL certs.

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var morgan = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var helmet = require('helmet');
 
@@ -22,7 +21,7 @@ var mongoose = require('mongoose');
 
 //connection to local Mongo Database
 
-mongoose.connect('mongodb://127.0.0.1/RoadTestDB', function(err, db){
+mongoose.connect('mongodb://127.0.0.1/RoadEstiCalcDB', function(err, db){
 	if (!err) {
 		console.log("Mongo Server port: Connected to App");
 	};
@@ -34,21 +33,22 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+var session = require('express-session');
 app.use(favicon(__dirname+'/build/favicon.ico'));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cookieParser());
+app.set('trust proxy',1) //trust first proxy
+app.use(session({secret: 'RoadE5t1mateT00L',
+				resave: false,
+				saveUninitialized: true,
+				cookie: {}}));
 
 //App is able to grab javscript, css, and images
 app.use(express.static(path.join(__dirname, 'build')));
 
-var session = require('express-session');
-var flash = require('connect-flash');
 
-app.use(session({secret: 'RoadE5t1mateT00L',
-				saveUninitialized: true,
-				resave: false}));
+var flash = require('connect-flash');
 app.use(flash());
 app.use(helmet());
 
